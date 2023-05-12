a986dad8cfac0b52b11ddccb8039171148bac0b5
Dorncy <zon528391@gmail.com>
2023-05-12 16:20:55 +0800

    test

--- index.py
+++ index.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from aiohttp import request
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
+# @app.route("/")
+# def index():
+#     return "test"
+
 def index():
-    return "test"
+    if request.method == 'POST':
+        name = request.form['name']
+        return f'Hello, {name}!'
+    return render_template('firebase_update.html')
 
 
 if __name__ == "__main__":
