c53078870a66a92e6d3139be62e7f89f1746df89
Dorncy <zon528391@gmail.com>
2023-05-12 17:00:11 +0800

    test

--- index.py
+++ index.py
@@ -2,15 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# @app.route("/")
-# def index():
-#     return "test"
-
 
 @app.route('/')
 def index():
-    return render_template("firebase_update.html")
+    return render_template('firebase_update.html')
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
